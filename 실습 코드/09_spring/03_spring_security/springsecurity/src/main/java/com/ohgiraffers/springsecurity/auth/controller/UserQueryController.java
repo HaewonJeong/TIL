fd0b1f9b4f2f81bd0853f9bc8e6f8cdb53b60ca2
HaewonJeong <bonjour_haewon@naver.com>
2026-06-24 11:54:36 +0900

    2026-06-24

---
+++ 실습 코드/09_spring/03_spring_security/springsecurity/src/main/java/com/ohgiraffers/springsecurity/auth/controller/UserQueryController.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.springsecurity.auth.controller;
+
+public class UserQueryController {
+}
---
+++ 실습 코드/09_spring/03_spring_security/springsecurity/src/main/java/com/ohgiraffers/springsecurity/query/dto/UserDto.java
@@ -0,0 +1,4 @@
+package com.ohgiraffers.springsecurity.query.dto;
+
+public class UserDto {
+}
